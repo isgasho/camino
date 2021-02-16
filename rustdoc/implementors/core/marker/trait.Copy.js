fee40398696d539b93ff4f0a5098f678b798acb2
sunshowers <rain@sunshowers.io>
2021-02-16 04:07:02 +0000

    Deploying to gh-pages from  @ d5e7f0bd25802a1c46426fde27bef9e12e9c15bc 🚀

--- rustdoc/implementors/core/marker/trait.Copy.js
+++ rustdoc/implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["camino"] = [{"text":"impl&lt;'a&gt; Copy for Utf8Ancestors&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Copy for Utf8Component&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Copy for Utf8PrefixComponent&lt;'a&gt;","synthetic":false,"types":[]}];
+implementors["camino"] = [{"text":"impl&lt;'a&gt; Copy for Utf8Ancestors&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Copy for Utf8Component&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Copy for Utf8Prefix&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Copy for Utf8PrefixComponent&lt;'a&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
